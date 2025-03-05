6a053706e9e38c682f66b731aac73d4dcfb8872a
mahdi <kashfimahdi90@gmail.com>
2025-03-06 03:00:38 +0330

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,92 @@ import ProductCard from "./components/ProductCard";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+//style
+import styles from "./App.module.css";
+
 function App() {
 
   return (
-    <>
-      <Header props={"title,price,img"} />
-      <ProductCard />
+    
+    <div >
+      <Header />
+      <div className={styles.container}>
+      <ProductCard 
+        title="Camera"
+        price="500"
+        image="/camera1.webp"
+        isDiscount={true}
+        quantity={15}
+        rate={4}
+        sp={true}//Special badge 
+      />
+      <ProductCard 
+        title="Camera"
+        price="500"
+        image="/camera2.webp"
+        isDiscount={true}
+        quantity={15}
+        rate={4}
+        sp={false}//Special badge 
+      />
+          <ProductCard 
+        title="Camera"
+        price="500"
+        image="/camera3.webp"
+        isDiscount={true}
+        quantity={4}
+        rate={4}
+        sp={false}//Special badge 
+      />
+          <ProductCard 
+        title="Camera"
+        price="500"
+        image="/camera4.webp"
+        isDiscount={true}
+        quantity={5}
+        rate={4}
+        sp={true}//Special badge 
+      />
+          <ProductCard 
+        title="Camera"
+        price="500"
+        image="/camera5.webp"
+        isDiscount={false}
+        quantity={15}
+        rate={4}
+        sp={true}//Special badge 
+      />
+      <ProductCard 
+        title="Camera"
+        price="500"
+        image="/camera6.webp"
+        isDiscount={false}
+        quantity={15}
+        rate={4}
+        sp={false}//Special badge 
+      />
+      <ProductCard 
+        title="Camera"
+        price="500"
+        image="/camera7.webp"
+        isDiscount={false}
+        quantity={5}
+        rate={4}
+        sp={true}//Special badge 
+      />
+          <ProductCard 
+        title="Camera"
+        price="500"
+        image="/camera8.webp"
+        isDiscount={false}
+        quantity={5}
+        rate={4}
+        sp={false}//Special badge 
+      />
+      </div>
       <Footer />
-    </>
+    </div> 
+   
   )
 }
 
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,19 +1,39 @@
-import React from 'react'
+import React from "react";
+import styles from "./ProductCard.module.css";
 
- function ProductCard() {
-
-
-
-
+function ProductCard({ title, price, image, isDiscount, quantity, rate, sp }) {
   return (
-    <div>
-        <div>
-            <img src='/Camera.jpg'/>
-            <h2>Camera</h2>
-            <h3>Price</h3>
-        </div>
+    <div className={styles.container}>
+      {sp && <p className={styles.sp}>SPECIAL OFFER</p>}
+      <img className={styles.img} src={image} />
+      <h2 className={styles.title}>{title}</h2>
+      <div className={styles.detail}>
+        <p className={styles.rate}>
+          <i className={`fa-solid fa-star ${styles.star}`}></i> {rate}{" "}
+        </p>
+
+        {quantity < 10 && quantity > 0 ? (
+          <p className={styles.quant}>
+            {" "}
+            تنها {quantity} عدد در انبار باقی مانده
+          </p>
+        ) : (
+          <p></p>
+        )}
+      </div>
+      <div className={styles.takhfif}>
+        {isDiscount ? (
+          <p className={styles.newPrice}>
+            <h2>{price - (price / 100) * 20}$</h2>
+            <h2 className={styles.darsad}>20%</h2>
+          </p>
+        ) : (
+          <h3>{price}$</h3>
+        )}
+        {isDiscount && <p className={styles.oldPrice}>{price}$</p>}
+      </div>
     </div>
-  )
+  );
 }
 
 export default ProductCard;
